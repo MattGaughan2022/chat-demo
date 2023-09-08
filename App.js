19ff969a694f5f416ad0fbcadb43150403ede5e5
Matt Gaughan <mattgaugs@gmail.com>
2023-09-08 09:54:56 -0400

    shopping list integration

--- App.js
+++ App.js
@@ -4,26 +4,62 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
+//firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+//components
 import Start from './components/Start';
 import Chat from './components/Chat';
+import ShoppingLists from './components/ShoppingLists';
+import Welcome from './components/Welcome';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
+  // Import the functions you need from the SDKs you need
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD-N3SdRqOA0zJ4qv_cx4Rj5ArYsTqcl6g",
+    authDomain: "shopping-list-demo-dbd8a.firebaseapp.com",
+    projectId: "shopping-list-demo-dbd8a",
+    storageBucket: "shopping-list-demo-dbd8a.appspot.com",
+    messagingSenderId: "633192329280",
+    appId: "1:633192329280:web:4f94864d2fb8edfa9e2fa5"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator
-      initialRouteName="Start"
+        initialRouteName="Welcome"
       >
-        <Stack.Screen
-        name="Start"
-        component={Start}
+        {/* initialRouteName="Start" */}
+        <Stack.Screen 
+        name="Welcome" 
+        component={Welcome} 
         />
         <Stack.Screen
-        name="Chat"
-        component={Chat}
+          name="Start"
+          component={Start}
         />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+        />
+        <Stack.Screen
+          name="ShoppingLists">
+          {props => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/Chat.js
+++ components/Chat.js
@@ -2,75 +2,76 @@ import { useEffect, useState } from 'react';
 import { StyleSheet, View, KeyboardAvoidingView, Platform, Text } from 'react-native';
 import { Bubble, GiftedChat } from "react-native-gifted-chat"; //find props from their repo
 
-const Chat = ({route, navigation}) => {
-    const {name} = route.params;
-    
-    const [messages, setMessages]=useState([]);
+const Chat = ({ route, navigation }) => {
+  const { name } = route.params;
 
-    const onSend = (newMessages) => {
-      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-    }
-    const renderBubble = (props) => {
-      return <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: "#000"
-          },
-          left: {
-            backgroundColor: "#FFF"
-          }
-        }}
-      />
-    }
+  const [messages, setMessages] = useState([]);
 
-    useEffect(()=> {
-      setMessages([
-        {
-          _id: 1,
-          text: "Hello Developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            // avatar: "placeholder.png",
-          },
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
         },
-        {
-          _id: 2,
-          text: 'this is a system message',
-          createdAt: new Date(),
-          system: true,
+        left: {
+          backgroundColor: "#FFF"
         }
-      ]);
-    }, []);
-
-    useEffect(()=> {
-        navigation.setOptions({ title: name });
-    }, []);
-
- return (
-   <View style={[styles.container, {backgroundColor: route.params.color}]}>
-     {/* <Text>Hello Chat Page!</Text>
-     <Text>Color: {route.params.color}</Text> */}
-     <GiftedChat
-      renderBubble={renderBubble}
-      messages={messages}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: 1
       }}
     />
-    <View style={{paddingBottom: 10}}></View>
-    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
-   </View>
- );
+  }
+
+  // setMessages([
+  //   {
+  //     _id: 1,
+  //     text: "Hello Developer",
+  //     createdAt: new Date(),
+  //     user: {
+  //       _id: 2,
+  //       name: "React Native",
+  //       // avatar: "placeholder.png",
+  //     },
+  //   },
+  //   {
+  //     _id: 2,
+  //     text: 'this is a system message',
+  //     createdAt: new Date(),
+  //     system: true,
+  //   }
+  // ]);
+
+  useEffect(() => {
+  }, []);
+
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+  }, []);
+
+  return (
+    <View style={[styles.container, { backgroundColor: route.params.color }]}>
+      {/* <Text>Hello Chat Page!</Text>
+     <Text>Color: {route.params.color}</Text> */}
+      <GiftedChat
+        renderBubble={renderBubble}
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+      <View style={{ paddingBottom: 10 }}></View>
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
- } 
+  container: {
+    flex: 1,
+  }
 });
 
 export default Chat;
