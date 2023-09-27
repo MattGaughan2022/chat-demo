23cc895640750ca88977afc70037bcbc2378d6bb
Matt Gaughan <mattgaugs@gmail.com>
2023-09-27 11:24:22 -0400

    full implementation

---
+++ README.md
@@ -0,0 +1,25 @@
+setup
+
+APP DEPLOYMENT SETUP
+
+1.  nvm install 16.19.0
+    nvm use 16.19.0
+2.  npm install -g expo-cli
+3.  create expo account at https://expo.dev/signup, use
+    expo login and follow credential process
+
+4.  clone and enter repo, run command 'npx expo start' and use either a mobile phone's 'Expo Go' development app, OR use an emulator (Android Studio) to simulate a mobile phone. To initialize your own database, proceed to the next section...
+
+FIRESTORE DATABASE SETUP
+
+in order to test a new database, enter the repository on your device and follow these steps
+
+1. https://console.firebase.google.com/ sign up or log in to create a firestore database
+
+2. create a project and under the 'Project Settings' (cog wheel) go to the 'General' tab, and find a selection called 'Your apps.'
+
+3. Generate a configuration for 'Firestore for Web' (may be shown as the </> icon). Follow the steps to add Firebase to your web app. This will generaste a config. code that will be used to wirelessly connect the app to the database.
+
+![Alt text](image.png)
+
+4. Copy and paste the text within the 'FirebaseConfig' variable in the web page and replace the text within the FirebaseConfig variable that is in the App.js file. Rerun the app using 'npx expo start' and enter a name to sign in and begin adding messages or photos to the database!
--- components/Start.js
+++ components/Start.js
@@ -1,13 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
-  StyleSheet, View, Text, Button,
-  TextInput, ImageBackground, TouchableOpacity, Alert
-} from 'react-native';
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { getAuth, signInAnonymously } from "firebase/auth";
 
 const Start = ({ navigation }) => {
-  const [name, setName] = useState('');
-  const [bgColor, setBgColor] = useState('#090C08');
+  const [name, setName] = useState("");
+  const [bgColor, setBgColor] = useState("#090C08");
 
   const auth = getAuth();
 
@@ -17,58 +23,99 @@ const Start = ({ navigation }) => {
         navigation.navigate("Chat", {
           userID: result.user.uid,
           name: name,
-          color: bgColor
+          color: bgColor,
         });
         Alert.alert("Signed in Successfully!");
       })
       .catch((error) => {
         Alert.alert("Unable to sign in, try later again.");
-      })
-  }
+      });
+  };
 
   const chooseBg = (color) => {
-    setBgColor(color)
-  }
+    setBgColor(color);
+  };
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('../bkimg.png')} style={styles.image}>
+      <ImageBackground source={require("../bkimg.png")} style={styles.image}>
         <View style={styles.content}>
           <Text style={styles.appTitle}>Chat App</Text>
           <TextInput
             style={styles.textInput}
             value={name}
             onChangeText={setName}
-            placeholder='Type username here'
-            placeholderTextColor='#FFFFFF'
-            color='#FFFFFF'
-            fontWeight='600'
+            placeholder="Type username here"
+            placeholderTextColor="#FFFFFF"
+            color="#FFFFFF"
+            fontWeight="600"
             require
           />
-          <Text style={{ color: '#FFFFFF' }}>Choose CHAT background color:</Text>
+          <Text style={{ color: "#FFFFFF" }}>
+            Choose CHAT background color:
+          </Text>
           <View style={styles.bgSection}>
             <TouchableOpacity
+              id="1"
               onPress={() => {
-                chooseBg('#090C08');
+                chooseBg("#090C08");
               }}
-              style={[styles.squareButton, { backgroundColor: '#090C08' }]} />
+              style={[
+                styles.squareButton,
+                { backgroundColor: "#090C08" },
+                [bgColor == "#090C08" ? { borderColor: "green" } : null],
+              ]}
+            >
+              {bgColor == "#090C08" ? (
+                <Text style={{ color: "white", fontSize: 40 }}>X</Text>
+              ) : null}
+            </TouchableOpacity>
             <TouchableOpacity
+              id="2"
               onPress={() => {
-                chooseBg('#474056');
+                chooseBg("#474056");
               }}
-              style={[styles.squareButton, { backgroundColor: '#474056' }]} />
+              style={[
+                styles.squareButton,
+                { backgroundColor: "#474056" },
+                [bgColor == "#474056" ? { borderColor: "green" } : null],
+              ]}
+            >
+              {bgColor == "#474056" ? (
+                <Text style={{ color: "white", fontSize: 40 }}>X</Text>
+              ) : null}
+            </TouchableOpacity>
             <TouchableOpacity
+              id="3"
               onPress={() => {
-                chooseBg('#8A95A5');
+                chooseBg("#8A95A5");
               }}
-              style={[styles.squareButton, { backgroundColor: '#8A95A5' }]} />
+              style={[
+                styles.squareButton,
+                { backgroundColor: "#8A95A5" },
+                [bgColor == "#8A95A5" ? { borderColor: "green" } : null],
+              ]}
+            >
+              {bgColor == "#8A95A5" ? (
+                <Text style={{ color: "white", fontSize: 40 }}>X</Text>
+              ) : null}
+            </TouchableOpacity>
             <TouchableOpacity
+              id="4"
               onPress={() => {
-                chooseBg('#B9C6AE');
+                chooseBg("#B9C6AE");
               }}
-              style={[styles.squareButton, { backgroundColor: '#B9C6AE' }]} />
+              style={[
+                styles.squareButton,
+                { backgroundColor: "#B9C6AE" },
+                [bgColor == "#B9C6AE" ? { borderColor: "green" } : null],
+              ]}
+            >
+              {bgColor == "#B9C6AE" ? (
+                <Text style={{ color: "white", fontSize: 40 }}>X</Text>
+              ) : null}
+            </TouchableOpacity>
           </View>
-          {/* #090C08; #474056; #8A95A5; #B9C6AE */}
           <TouchableOpacity
             accessible={true}
             accessibilityLabel="Go to chat page"
@@ -76,12 +123,14 @@ const Start = ({ navigation }) => {
             accessibilityRole="button"
             style={styles.button}
             onPress={signInUser}
-          ><Text style={{ color: '#FFFFFF' }}>Start Chatting</Text></TouchableOpacity>
+          >
+            <Text style={{ color: "#FFFFFF" }}>Start Chatting</Text>
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -93,20 +142,20 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginTop: 15,
     marginBottom: 15,
-    borderColor: 'white',
+    borderColor: "white",
     fontSize: 16,
     fontWeight: 300,
-    color: '#757083',
+    color: "#757083",
   },
   appTitle: {
     fontSize: 45,
-    fontWeight: '600',
-    color: '#FFFFFF',
+    fontWeight: "600",
+    color: "#FFFFFF",
   },
   content: {
     paddingTop: 80,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   image: {
     flex: 1,
@@ -114,23 +163,25 @@ const styles = StyleSheet.create({
   button: {
     padding: 5,
     borderWidth: 2,
-    borderColor: '#FFFFFF',
-    backgroundColor: '#757083',
+    borderColor: "#FFFFFF",
+    backgroundColor: "#757083",
     fontWeight: 600,
     fontSize: 16,
   },
   bgSection: {
     paddingTop: 15,
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingBottom: 15,
   },
   squareButton: {
-    padding: 35,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 80,
+    width: 80,
     borderWidth: 2,
     borderRadius: 22,
-    borderColor: '#FFFFFF',
-
-  }
+    borderColor: "#FFFFFF",
+  },
 });
 
 export default Start;
